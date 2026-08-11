0ff29a44aac390a0595d2e3485e7d2afa40dbd0d
Sam <sammyphala99@gmail.com>
2026-08-11 09:06:47 +0200

    Update email sender details in resendServices.js

--- services/resendServices.js
+++ services/resendServices.js
@@ -5,8 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (address, message, subject) => {
     const { data, error } = await resend.emails.send({
-        from: 'NN motors <onboarding@resend.dev>',
-        to: [address],
+        from: 'onboarding@resend.dev',
+        to: 'sammyphala99@gmail.com',
         subject: subject,
         html: message,
     })
