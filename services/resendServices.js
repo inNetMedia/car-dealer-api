dcc89ed48106cd7f30aeafb8d1ee1a96fc6d5b59
Sam P <159704859+inNetMedia@users.noreply.github.com>
2026-08-24 10:00:28 +0200

    updated resend due to domain reg issue

--- services/resendServices.js
+++ services/resendServices.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async (address, message, subject) => {
     const { data, error } = await resend.emails.send({
         from: 'onboarding@resend.dev',
-        to: address,
+        to: 'sammyphala99@gmail.com',         //Domain not registered, change to {to: address } upon registering your domain to resend.com. 
         subject: subject,
         html: message,
     })
